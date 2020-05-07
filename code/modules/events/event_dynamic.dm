Baystation12/Baystation12

Adds rogue singularity event.

code/modules/events/event_dynamic.dm
@@ -101,6 +101,7 @@ var/list/event_last_fired = list()
 	active_with_role["Robot"] = 0
 	active_with_role["Janitor"] = 0
 	active_with_role["Gardener"] = 0
+	active_with_role["Command"] = 0
 
 	for(var/mob/M in GLOB.player_list)
 		if(!M.mind || !M.client || M.client.is_afk(10 MINUTES)) // longer than 10 minutes AFK counts them as inactive
@@ -131,6 +132,9 @@ var/list/event_last_fired = list()
 
 		if(M.mind.assigned_role in SSjobs.titles_by_department(SCI))
 			active_with_role["Scientist"]++
+
+		if(M.mind.assigned_role in SSjobs.titles_by_department(COM))
+			active_with_role["Command"]++
 
 		if(M.mind.assigned_role == "AI")
 			active_with_role["AI"]++

NebulaSS13/Nebula

Conflicts:
	baystation12.dme
	icons/obj/overmap.dmi

code/modules/events/event_dynamic.dm
@@ -101,6 +101,7 @@ var/list/event_last_fired = list()
 	active_with_role["Robot"] = 0
 	active_with_role["Janitor"] = 0
 	active_with_role["Gardener"] = 0
+	active_with_role["Command"] = 0
 
 	for(var/mob/M in GLOB.player_list)
 		if(!M.mind || !M.client || M.client.is_afk(10 MINUTES)) // longer than 10 minutes AFK counts them as inactive
@@ -131,6 +132,9 @@ var/list/event_last_fired = list()
 
 		if(M.mind.assigned_role in SSjobs.titles_by_department("science"))
 			active_with_role["Scientist"]++
+
+		if(M.mind.assigned_role in SSjobs.titles_by_department(DEPT_COMMAND))
+			active_with_role["Command"]++
 
 		if(M.mind.assigned_role == "AI")
 			active_with_role["AI"]++