Baystation12/Baystation12

Add some pAI Hud

code/_onclick/hud/_defines.dm
@@ -124,3 +124,11 @@
 
 #define ui_spell_master "EAST-1:16,NORTH-1:16"
 #define ui_genetic_master "EAST-1:16,NORTH-3:16"
+
+//pai
+#define ui_pai_software "NORTH,WEST:6"
+#define ui_pai_subsystems "NORTH,WEST+1:6"
+#define ui_pai_shell "NORTH,WEST+2:6"
+#define ui_pai_light "NORTH,WEST+3:6"
+#define ui_pai_rest "NORTH,WEST+4:6"
+

code/modules/client/preference_setup/global/03_pai.dm
@@ -2,7 +2,10 @@
 	name = "pAI"
 	sort_order = 3
 
+	var/icon/pai_preview
 	var/datum/paiCandidate/candidate
+	var/icon/bgstate = MATERIAL_STEEL
+	var/list/bgstate_options = list("FFF", MATERIAL_STEEL, "white")
 
 /datum/category_item/player_setup_item/player_global/pai/load_preferences(var/savefile/S)
 	if(!candidate)
@@ -12,6 +15,7 @@
 		return
 
 	candidate.savefile_load(preference_mob())
+	update_pai_preview()
 
 /datum/category_item/player_setup_item/player_global/pai/save_preferences(var/savefile/S)
 	if(!candidate)
@@ -25,7 +29,7 @@
 /datum/category_item/player_setup_item/player_global/pai/content(var/mob/user)
 	if(!candidate)
 		candidate = new()
-
+	update_pai_preview()
 	. += "<b>pAI:</b><br>"
 	if(!candidate)
 		log_debug("[user] pAI prefs have a null candidate var.")
@@ -34,6 +38,10 @@
 	. += "Description: <a href='?src=\ref[src];option=desc'>[candidate.description ? TextPreview(candidate.description, 40) : "None Set"]</a><br>"
 	. += "Role: <a href='?src=\ref[src];option=role'>[candidate.role ? TextPreview(candidate.role, 40) : "None Set"]</a><br>"
 	. += "OOC Comments: <a href='?src=\ref[src];option=ooc'>[candidate.comments ? TextPreview(candidate.comments, 40) : "None Set"]</a><br>"
+	. += "<div>Chassis: <a href='?src=\ref[src];option=chassis'>[candidate.chassis]</a><br>"
+	. += "<div>Say Verb: <a href='?src=\ref[src];option=say'>[candidate.say_verb]</a><br>"
+	. += "<table><tr style='vertical-align:top'><td><div class='statusDisplay'><center><img src=pai_preview.png width=[pai_preview.Width()] height=[pai_preview.Height()]></center><a href='?src=\ref[src];option=cyclebg'>Cycle Background</a></div>"
+	. += "</td></tr></table>"
 
 /datum/category_item/player_setup_item/player_global/pai/OnTopic(var/href,var/list/href_list, var/mob/user)
 	if(href_list["option"])
@@ -55,6 +63,30 @@
 				t = input(user, "Enter any OOC comments", "Global Preference", html_decode(candidate.comments)) as message
 				if(!isnull(t) && CanUseTopic(user))
 					candidate.comments = sanitize(t)
+			if("chassis")
+				candidate.chassis = input(usr,"What would you like to use for your mobile chassis icon?") as null|anything in GLOB.possible_chassis
+				update_pai_preview()
+			if("say")
+				candidate.say_verb = input(usr,"What theme would you like to use for your speech verbs?") as null|anything in GLOB.possible_say_verbs
+			if("cyclebg")
+				bgstate = next_in_list(bgstate, bgstate_options)
+				update_pai_preview()
+
 		return TOPIC_REFRESH
 
 	return ..()
+
+/datum/category_item/player_setup_item/player_global/pai/proc/update_pai_preview()
+	pai_preview = icon('icons/effects/128x48.dmi', bgstate)
+	var/icon/pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], NORTH)
+	pai_preview.Scale(48+32, 16+32)
+
+	pai_preview.Blend(pai, ICON_OVERLAY, 25, 22)
+	pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], WEST)
+	pai_preview.Blend(pai, ICON_OVERLAY, 1, 9)
+	pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], SOUTH)
+	pai_preview.Blend(pai, ICON_OVERLAY, 49, 5)
+
+	pai_preview.Scale(pai_preview.Width() * 2, pai_preview.Height() * 2)
+
+	send_rsc(usr, pai_preview, "pai_preview.png")

NebulaSS13/Nebula

code/_onclick/hud/_defines.dm
@@ -156,3 +156,10 @@
 // AI: Crew
 #define ui_ai_crew_mon "RIGHT-1:30,TOP:0"
 #define ui_ai_crew_rec "RIGHT-2:30, TOP:0"
+
+// pAI
+#define ui_pai_software "TOP,LEFT:6"
+#define ui_pai_subsystems "TOP,LEFT+1:6"
+#define ui_pai_shell "TOP,LEFT+2:6"
+#define ui_pai_light "TOP,LEFT+3:6"
+#define ui_pai_rest "TOP,LEFT+4:6"

code/modules/client/preference_setup/global/03_pai.dm
@@ -2,7 +2,10 @@
 	name = "pAI"
 	sort_order = 3
 
+	var/icon/pai_preview
 	var/datum/paiCandidate/candidate
+	var/icon/bgstate = DEFAULT_WALL_MATERIAL
+	var/list/bgstate_options = list("FFF", DEFAULT_WALL_MATERIAL, "white")
 
 /datum/category_item/player_setup_item/player_global/pai/load_preferences(var/savefile/S)
 	if(!candidate)
@@ -12,6 +15,7 @@
 		return
 
 	candidate.savefile_load(preference_mob())
+	update_pai_preview()
 
 /datum/category_item/player_setup_item/player_global/pai/save_preferences(var/savefile/S)
 	if(!candidate)
@@ -26,6 +30,9 @@
 	if(!candidate)
 		candidate = new()
 
+	if (!pai_preview)
+		update_pai_preview(user)
+
 	. += "<b>pAI:</b><br>"
 	if(!candidate)
 		log_debug("[user] pAI prefs have a null candidate var.")
@@ -34,6 +41,10 @@
 	. += "Description: <a href='?src=\ref[src];option=desc'>[candidate.description ? TextPreview(candidate.description, 40) : "None Set"]</a><br>"
 	. += "Role: <a href='?src=\ref[src];option=role'>[candidate.role ? TextPreview(candidate.role, 40) : "None Set"]</a><br>"
 	. += "OOC Comments: <a href='?src=\ref[src];option=ooc'>[candidate.comments ? TextPreview(candidate.comments, 40) : "None Set"]</a><br>"
+	. += "<div>Chassis: <a href='?src=\ref[src];option=chassis'>[candidate.chassis]</a><br>"
+	. += "<div>Say Verb: <a href='?src=\ref[src];option=say'>[candidate.say_verb]</a><br>"
+	. += "<table><tr style='vertical-align:top'><td><div class='statusDisplay'><center><img src=pai_preview.png width=[pai_preview.Width()] height=[pai_preview.Height()]></center><a href='?src=\ref[src];option=cyclebg'>Cycle Background</a></div>"
+	. += "</td></tr></table>"
 
 /datum/category_item/player_setup_item/player_global/pai/OnTopic(var/href,var/list/href_list, var/mob/user)
 	if(href_list["option"])
@@ -55,6 +66,34 @@
 				t = input(user, "Enter any OOC comments", "Global Preference", html_decode(candidate.comments)) as message
 				if(!isnull(t) && CanUseTopic(user))
 					candidate.comments = sanitize(t)
+			if("chassis")
+				t = input(usr,"What would you like to use for your mobile chassis icon?") as null|anything in GLOB.possible_chassis
+				if(!isnull(t) && CanUseTopic(user))
+					candidate.chassis = t
+				update_pai_preview(user)
+			if("say")
+				t = input(usr,"What theme would you like to use for your speech verbs?") as null|anything in GLOB.possible_say_verbs
+				if(!isnull(t) && CanUseTopic(user))
+					candidate.say_verb = t
+			if("cyclebg")
+				bgstate = next_in_list(bgstate, bgstate_options)
+				update_pai_preview(user)
+
 		return TOPIC_REFRESH
 
 	return ..()
+
+/datum/category_item/player_setup_item/player_global/pai/proc/update_pai_preview(mob/user)
+	pai_preview = icon('icons/effects/128x48.dmi', bgstate)
+	var/icon/pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], NORTH)
+	pai_preview.Scale(48+32, 16+32)
+
+	pai_preview.Blend(pai, ICON_OVERLAY, 25, 22)
+	pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], WEST)
+	pai_preview.Blend(pai, ICON_OVERLAY, 1, 9)
+	pai = icon('icons/mob/pai.dmi', GLOB.possible_chassis[candidate.chassis], SOUTH)
+	pai_preview.Blend(pai, ICON_OVERLAY, 49, 5)
+
+	pai_preview.Scale(pai_preview.Width() * 2, pai_preview.Height() * 2)
+
+	send_rsc(user, pai_preview, "pai_preview.png")