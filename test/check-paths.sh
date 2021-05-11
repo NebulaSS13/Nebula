0607acb4d5cc0c5c5367f9599399e41ff7931f44
Lohikar <lohikar@protonmail.com>
2021-05-11 15:07:09 -0500

    Allow const use

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -40,7 +40,7 @@ exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
 exactly 1 "goto uses" 'goto '
 exactly 6 "atom/New uses" '^/(obj|atom|area|mob|turf).*/New\('
 exactly 0 "tag uses" '\stag = ' -P '**/*.dmm'
-exactly 3 "unmarked globally scoped variables" -P '^(/|)var/(?!global)'
+exactly 3 "unmarked globally scoped variables" -P '^(/|)var/(?!(global|const))'
 exactly 0 "global-marked member variables" -P '\t(/|)var.*/global/.+'
 exactly 0 "static-marked globally scoped variables" -P '^(/|)var.*/static/.+'
 
