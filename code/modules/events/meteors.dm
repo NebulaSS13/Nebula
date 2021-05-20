Baystation12/Baystation12

Makes higher piloting skills matter for meteors
a

code/modules/events/meteors.dm
@@ -110,27 +110,42 @@
 	. = ..()
 
 /datum/event/meteor_wave/overmap/tick()
-	if(victim && !victim.is_still()) //Meteors mostly fly in your face
-		start_side = prob(90) ? victim.fore_dir : pick(GLOB.cardinal)
-	else //Unless you're standing
+	if(!victim)
+		return
+	if (victim.is_still() || victim.get_helm_skill() >= SKILL_ADEPT) //Unless you're standing or good at your job..
 		start_side = pick(GLOB.cardinal)
+	else //..Meteors mostly fly in your face
+		start_side = prob(90) ? victim.fore_dir : pick(GLOB.cardinal)
 	..()
 
 /datum/event/meteor_wave/overmap/get_wave_size()
 	. = ..()
-	if(!victim)
+	if (!victim)
 		return
 	var/skill = victim.get_helm_skill()
 	var/speed = victim.get_speed()
-	if(skill >= SKILL_PROF)
-		. = round(. * 0.5)
-	if(victim.is_still()) //Standing still means less shit flies your way
-		. = round(. * 0.1)
-	if(speed < SHIP_SPEED_SLOW) //Slow and steady
-		. = round(. * 0.5)
-	if(speed > SHIP_SPEED_FAST) //Sanic stahp
-		. *= 2
-	
+	if (skill < SKILL_EXPERT)
+		if(victim.is_still() || speed < SHIP_SPEED_SLOW) //Standing still or being slow means less shit flies your way
+			. = round(. * 0.7)
+		if(speed > SHIP_SPEED_FAST) //Sanic stahp
+			. *= 2
+	if (skill == SKILL_EXPERT)
+		if (victim.is_still())
+			. = round(. * 0.2)
+		if (speed < SHIP_SPEED_SLOW)
+			. = round(. * 0.5)
+		if (speed > SHIP_SPEED_SLOW && speed < SHIP_SPEED_FAST)
+			. = round(. * 0.7)
+		if (speed > SHIP_SPEED_FAST)
+			. = round(. * 1.2)
+	if (skill > SKILL_EXPERT)
+		if (victim.is_still())
+			. = round(. * 0.1)
+		if (speed < SHIP_SPEED_SLOW)
+			. = round(. * 0.2)
+		if (speed > SHIP_SPEED_SLOW && speed < SHIP_SPEED_FAST)
+			. = round(. * 0.5)
+
 	//Smol ship evasion
 	if(victim.vessel_size < SHIP_SIZE_LARGE && speed < SHIP_SPEED_FAST)
 		var/skill_needed = SKILL_PROF

code/modules/mob/skills/skill.dm
@@ -125,9 +125,9 @@ GLOBAL_LIST_EMPTY(skills)
 	desc = "Describes your experience and understanding of piloting spacecraft, from small and short-range pods to corvette sized vessels."
 	levels = list( "Unskilled"			= "You know what a spacecraft is, and you might have an abstract understanding of the differences between various ships. If your department is involved in the use of spacecraft, you know roughly what their capabilities are. You might be able to fly a spacecraft in a videogame. If you were to take the Helm of a smaller vessel, you might be able to move it with proper guidance.<br>- Travel time between tranisition decreases with level.<br>- You can fly ships but their movement might be randomized.<br>- The speed of your ship hitting carps will increase with level.",
 						"Basic"				= "You can pilot a small, short-range craft safely, but larger ships are out of your area of expertise. You are by no means an expert, and probably don't have much training. Skills of this level are typical for deck crew.<br>- You can operate small shuttlecraft without error.<br>- You can completely avoid meteors on slow speed while using tiny shuttlecrafts such as the GUP.",
-						"Trained"			= "You are a trained pilot, and can safely operate anything from a small craft to a corvette. You can spend extended periods of time piloting a spacecraft, and you're versed in the abilities of different ships, and what makes them function. You can do basic maintenance on smaller vessels, and perform most basic maneuvers. You can use armed spacecraft. You can make basic calculations relating to piloting. Skills of this level are typical for newer pilots. You have probably received formal piloting training.<br>- You can operate large ships without error.<br>- You can completely avoid meteors on slow speed using any shuttlecrafts.",
-						"Experienced"		= "You are an experienced pilot, and can safely take the helm of many types of craft. You could probably live in a spacecraft, and you're very well versed in essentially everything related to space-faring vessels. Not only can you fly a ship, but you can perform difficult maneuvers, and make most calculations related to piloting a spacecraft. You can maintain a ship. Skills of this level are typical for very experienced pilots. You have received formal piloting training.<br>- You can completely avoid meteors on normal speed while using tiny shuttlecrafts.",
-						"Master"		= "Not only are you an exceptional pilot, but you have mastered peripheral functions such as stellar navigation and bluespace jump plotting. You have experience performing complex maneuvers, managing squadrons of small craft, and operating in hostile environments.<br>- You can completely avoid meteors on normal speed using any shuttlecrafts.<br>- Less meteors will hit the ship while passing through meteor fields.")
+						"Trained"			= "You are a trained pilot, and can safely operate anything from a small craft to a corvette. You can spend extended periods of time piloting a spacecraft, and you're versed in the abilities of different ships, and what makes them function. You can do basic maintenance on smaller vessels, and perform most basic maneuvers. You can use armed spacecraft. You can make basic calculations relating to piloting. Skills of this level are typical for newer pilots. You have probably received formal piloting training.<br>- You can operate large ships without error.<br>- You can mostly avoid meteors on slow speed using any shuttlecrafts.",
+						"Experienced"		= "You are an experienced pilot, and can safely take the helm of many types of craft. You could probably live in a spacecraft, and you're very well versed in essentially everything related to space-faring vessels. Not only can you fly a ship, but you can perform difficult maneuvers, and make most calculations related to piloting a spacecraft. You can maintain a ship. Skills of this level are typical for very experienced pilots. You have received formal piloting training.<br>- You can somewhat avoid meteors on normal speed while using tiny shuttlecrafts.",
+						"Master"		= "Not only are you an exceptional pilot, but you have mastered peripheral functions such as stellar navigation and bluespace jump plotting. You have experience performing complex maneuvers, managing squadrons of small craft, and operating in hostile environments.<br>- You can mostly avoid meteors on normal speed using any shuttlecrafts.<br>- Less meteors will hit the ship while passing through meteor fields.")
 	difficulty = SKILL_AVERAGE
 	default_max = SKILL_ADEPT
 

NebulaSS13/Nebula

(cherry picked from commit f78b88706fa2b797b486c79854d03c7d7a2f7d32)

code/modules/events/meteors.dm
@@ -110,27 +110,42 @@ var/global/list/meteors_major = list(
 	. = ..()
 
 /datum/event/meteor_wave/overmap/tick()
-	if(victim && !victim.is_still()) //Meteors mostly fly in your face
-		start_side = prob(90) ? victim.fore_dir : pick(global.cardinal)
-	else //Unless you're standing
+	if(!victim)
+		return
+	if (victim.is_still() || victim.get_helm_skill() >= SKILL_ADEPT) //Unless you're standing or good at your job..
 		start_side = pick(global.cardinal)
+	else //..Meteors mostly fly in your face
+		start_side = prob(90) ? victim.fore_dir : pick(global.cardinal)
 	..()
 
 /datum/event/meteor_wave/overmap/get_wave_size()
 	. = ..()
-	if(!victim)
+	if (!victim)
 		return
 	var/skill = victim.get_helm_skill()
 	var/speed = victim.get_speed()
-	if(skill >= SKILL_PROF)
-		. = round(. * 0.5)
-	if(victim.is_still()) //Standing still means less shit flies your way
-		. = round(. * 0.1)
-	if(speed < SHIP_SPEED_SLOW) //Slow and steady
-		. = round(. * 0.5)
-	if(speed > SHIP_SPEED_FAST) //Sanic stahp
-		. *= 2
-	
+	if (skill < SKILL_EXPERT)
+		if(victim.is_still() || speed < SHIP_SPEED_SLOW) //Standing still or being slow means less shit flies your way
+			. = round(. * 0.7)
+		if(speed > SHIP_SPEED_FAST) //Sanic stahp
+			. *= 2
+	else if (skill == SKILL_EXPERT)
+		if (victim.is_still())
+			. = round(. * 0.2)
+		if (speed < SHIP_SPEED_SLOW)
+			. = round(. * 0.5)
+		else if (speed > SHIP_SPEED_SLOW && speed < SHIP_SPEED_FAST)
+			. = round(. * 0.7)
+		if (speed > SHIP_SPEED_FAST)
+			. = round(. * 1.2)
+	else if (skill > SKILL_EXPERT)
+		if (victim.is_still())
+			. = round(. * 0.1)
+		if (speed < SHIP_SPEED_SLOW)
+			. = round(. * 0.2)
+		else if (speed > SHIP_SPEED_SLOW && speed < SHIP_SPEED_FAST)
+			. = round(. * 0.5)
+
 	//Smol ship evasion
 	if(victim.vessel_size < SHIP_SIZE_LARGE && speed < SHIP_SPEED_FAST)
 		var/skill_needed = SKILL_PROF

code/modules/mob/skills/skill.dm
@@ -135,9 +135,9 @@ var/global/list/skills = list()
 	desc = "Describes your experience and understanding of piloting spacecraft, from small and short-range pods to corvette sized vessels."
 	levels = list( "Unskilled"			= "You know what a spacecraft is, and you might have an abstract understanding of the differences between various ships. If your department is involved in the use of spacecraft, you know roughly what their capabilities are. You might be able to fly a spacecraft in a videogame. If you were to take the Helm of a smaller vessel, you might be able to move it with proper guidance.<br>- Travel time between tranisition decreases with level.<br>- You can fly ships but their movement might be randomized.<br>- The speed of your ship hitting carps will increase with level.",
 						"Basic"				= "You can pilot a small, short-range craft safely, but larger ships are out of your area of expertise. You are by no means an expert, and probably don't have much training. Skills of this level are typical for deck crew.<br>- You can operate small shuttlecraft without error.<br>- You can completely avoid meteors on slow speed while using tiny shuttlecrafts such as the GUP.",
-						"Trained"			= "You are a trained pilot, and can safely operate anything from a small craft to a corvette. You can spend extended periods of time piloting a spacecraft, and you're versed in the abilities of different ships, and what makes them function. You can do basic maintenance on smaller vessels, and perform most basic maneuvers. You can use armed spacecraft. You can make basic calculations relating to piloting. Skills of this level are typical for newer pilots. You have probably received formal piloting training.<br>- You can operate large ships without error.<br>- You can completely avoid meteors on slow speed using any shuttlecrafts.",
-						"Experienced"		= "You are an experienced pilot, and can safely take the helm of many types of craft. You could probably live in a spacecraft, and you're very well versed in essentially everything related to space-faring vessels. Not only can you fly a ship, but you can perform difficult maneuvers, and make most calculations related to piloting a spacecraft. You can maintain a ship. Skills of this level are typical for very experienced pilots. You have received formal piloting training.<br>- You can completely avoid meteors on normal speed while using tiny shuttlecrafts.",
-						"Master"		= "Not only are you an exceptional pilot, but you have mastered peripheral functions such as stellar navigation and FTL jump plotting. You have experience performing complex maneuvers, managing squadrons of small craft, and operating in hostile environments.<br>- You can completely avoid meteors on normal speed using any shuttlecrafts.<br>- Less meteors will hit the ship while passing through meteor fields. <br>- You can manually land shuttles on exoplanets.")
+						"Trained"			= "You are a trained pilot, and can safely operate anything from a small craft to a corvette. You can spend extended periods of time piloting a spacecraft, and you're versed in the abilities of different ships, and what makes them function. You can do basic maintenance on smaller vessels, and perform most basic maneuvers. You can use armed spacecraft. You can make basic calculations relating to piloting. Skills of this level are typical for newer pilots. You have probably received formal piloting training.<br>- You can operate large ships without error.<br>- You can mostly avoid meteors on slow speed using any shuttlecrafts.",
+						"Experienced"		= "You are an experienced pilot, and can safely take the helm of many types of craft. You could probably live in a spacecraft, and you're very well versed in essentially everything related to space-faring vessels. Not only can you fly a ship, but you can perform difficult maneuvers, and make most calculations related to piloting a spacecraft. You can maintain a ship. Skills of this level are typical for very experienced pilots. You have received formal piloting training.<br>- You can somewhat avoid meteors on normal speed while using tiny shuttlecrafts.",
+						"Master"		= "Not only are you an exceptional pilot, but you have mastered peripheral functions such as stellar navigation and FTL jump plotting. You have experience performing complex maneuvers, managing squadrons of small craft, and operating in hostile environments.<br>- You can mostly avoid meteors on normal speed using any shuttlecrafts.<br>- Less meteors will hit the ship while passing through meteor fields. <br>- You can manually land shuttles on exoplanets.")
 	difficulty = SKILL_AVERAGE
 	default_max = SKILL_ADEPT
 