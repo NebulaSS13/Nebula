Baystation12/Baystation12

Replace savefile-based preferences with JSON files
Refactors preference loading/saving to be vaguely generic over the
underlying storage mechanism. New implementation is a JSON file per
character slot.
Should be more reliable and less prone to randomly corrupting/losing
preferences.
Savefile-based preferences will migrate automatically, as long as
they've been updated "recently" (since 2018) and are tied to the toch.
update_setup() is gone; version handling should now be done in load_*.

code/modules/client/preference_setup/general/01_basic.dm
@@ -10,21 +10,21 @@ datum/preferences
 	name = "Basic"
 	sort_order = 1
 
-/datum/category_item/player_setup_item/physical/basic/load_character(var/savefile/S)
-	from_save(S["gender"],                pref.gender)
-	from_save(S["age"],                   pref.age)
-	from_save(S["spawnpoint"],            pref.spawnpoint)
-	from_save(S["OOC_Notes"],             pref.metadata)
-	from_save(S["real_name"],             pref.real_name)
-	from_save(S["name_is_always_random"], pref.be_random_name)
+/datum/category_item/player_setup_item/physical/basic/load_character(datum/pref_record_reader/R)
+	pref.gender = R.read("gender")
+	pref.age = R.read("age")
+	pref.spawnpoint = R.read("spawnpoint")
+	pref.metadata = R.read("OOC_Notes")
+	pref.real_name = R.read("real_name")
+	pref.be_random_name = R.read("name_is_always_random")
 
-/datum/category_item/player_setup_item/physical/basic/save_character(var/savefile/S)
-	to_save(S["gender"],                  pref.gender)
-	to_save(S["age"],                     pref.age)
-	to_save(S["spawnpoint"],              pref.spawnpoint)
-	to_save(S["OOC_Notes"],               pref.metadata)
-	to_save(S["real_name"],               pref.real_name)
-	to_save(S["name_is_always_random"],   pref.be_random_name)
+/datum/category_item/player_setup_item/physical/basic/save_character(datum/pref_record_writer/W)
+	W.write("gender", pref.gender)
+	W.write("age", pref.age)
+	W.write("spawnpoint", pref.spawnpoint)
+	W.write("OOC_Notes", pref.metadata)
+	W.write("real_name", pref.real_name)
+	W.write("name_is_always_random", pref.be_random_name)
 
 /datum/category_item/player_setup_item/physical/basic/sanitize_character()
 	var/datum/species/S = all_species[pref.species ? pref.species : SPECIES_HUMAN]

code/modules/client/preference_setup/general/02_body.dm
@@ -38,58 +38,58 @@ var/global/list/valid_bloodtypes = list("A+", "A-", "B+", "B-", "AB+", "AB-", "O
 	sort_order = 2
 	var/hide_species = TRUE
 
-/datum/category_item/player_setup_item/physical/body/load_character(var/savefile/S)
-	from_save(S["species"], pref.species)
-	from_save(S["hair_red"], pref.r_hair)
-	from_save(S["hair_green"], pref.g_hair)
-	from_save(S["hair_blue"], pref.b_hair)
-	from_save(S["facial_red"], pref.r_facial)
-	from_save(S["facial_green"], pref.g_facial)
-	from_save(S["facial_blue"], pref.b_facial)
-	from_save(S["skin_tone"], pref.s_tone)
-	from_save(S["skin_red"], pref.r_skin)
-	from_save(S["skin_green"], pref.g_skin)
-	from_save(S["skin_blue"], pref.b_skin)
-	from_save(S["skin_base"], pref.s_base)
-	from_save(S["hair_style_name"], pref.h_style)
-	from_save(S["facial_style_name"], pref.f_style)
-	from_save(S["eyes_red"], pref.r_eyes)
-	from_save(S["eyes_green"], pref.g_eyes)
-	from_save(S["eyes_blue"], pref.b_eyes)
-	from_save(S["b_type"], pref.b_type)
-	from_save(S["disabilities"], pref.disabilities)
-	from_save(S["organ_data"], pref.organ_data)
-	from_save(S["rlimb_data"], pref.rlimb_data)
-	from_save(S["body_markings"], pref.body_markings)
-	from_save(S["body_descriptors"], pref.body_descriptors)
+/datum/category_item/player_setup_item/physical/body/load_character(datum/pref_record_reader/R)
+	pref.species = R.read("species")
+	pref.r_hair = R.read("hair_red")
+	pref.g_hair = R.read("hair_green")
+	pref.b_hair = R.read("hair_blue")
+	pref.r_facial = R.read("facial_red")
+	pref.g_facial = R.read("facial_green")
+	pref.b_facial = R.read("facial_blue")
+	pref.s_tone = R.read("skin_tone")
+	pref.r_skin = R.read("skin_red")
+	pref.g_skin = R.read("skin_green")
+	pref.b_skin = R.read("skin_blue")
+	pref.s_base = R.read("skin_base")
+	pref.h_style = R.read("hair_style_name")
+	pref.f_style = R.read("facial_style_name")
+	pref.r_eyes = R.read("eyes_red")
+	pref.g_eyes = R.read("eyes_green")
+	pref.b_eyes = R.read("eyes_blue")
+	pref.b_type = R.read("b_type")
+	pref.disabilities = R.read("disabilities")
+	pref.organ_data = R.read("organ_data")
+	pref.rlimb_data = R.read("rlimb_data")
+	pref.body_markings = R.read("body_markings")
+	pref.body_descriptors = R.read("body_descriptors")
 	pref.preview_icon = null
-	from_save(S["bgstate"], pref.bgstate)
+	pref.bgstate = R.read("bgstate")
 
-/datum/category_item/player_setup_item/physical/body/save_character(var/savefile/S)
-	to_save(S["species"], pref.species)
-	to_save(S["hair_red"], pref.r_hair)
-	to_save(S["hair_green"], pref.g_hair)
-	to_save(S["hair_blue"], pref.b_hair)
-	to_save(S["facial_red"], pref.r_facial)
-	to_save(S["facial_green"], pref.g_facial)
-	to_save(S["facial_blue"], pref.b_facial)
-	to_save(S["skin_tone"], pref.s_tone)
-	to_save(S["skin_red"], pref.r_skin)
-	to_save(S["skin_green"], pref.g_skin)
-	to_save(S["skin_base"], pref.s_base)
-	to_save(S["skin_blue"], pref.b_skin)
-	to_save(S["hair_style_name"],pref.h_style)
-	to_save(S["facial_style_name"],pref.f_style)
-	to_save(S["eyes_red"], pref.r_eyes)
-	to_save(S["eyes_green"], pref.g_eyes)
-	to_save(S["eyes_blue"], pref.b_eyes)
-	to_save(S["b_type"], pref.b_type)
-	to_save(S["disabilities"], pref.disabilities)
-	to_save(S["organ_data"], pref.organ_data)
-	to_save(S["rlimb_data"], pref.rlimb_data)
-	to_save(S["body_markings"], pref.body_markings)
-	to_save(S["body_descriptors"], pref.body_descriptors)
-	to_save(S["bgstate"], pref.bgstate)
+/datum/category_item/player_setup_item/physical/body/save_character(datum/pref_record_writer/W)
+	W.write("species", pref.species)
+	W.write("hair_red", pref.r_hair)
+	W.write("hair_green", pref.g_hair)
+	W.write("hair_blue", pref.b_hair)
+	W.write("facial_red", pref.r_facial)
+	W.write("facial_green", pref.g_facial)
+	W.write("facial_blue", pref.b_facial)
+	W.write("skin_tone", pref.s_tone)
+	W.write("skin_red", pref.r_skin)
+	W.write("skin_green", pref.g_skin)
+	W.write("skin_base", pref.s_base)
+	W.write("skin_blue", pref.b_skin)
+	W.write("hair_style_name", pref.h_style)
+	W.write("facial_style_name", pref.f_style)
+	W.write("eyes_red", pref.r_eyes)
+	W.write("eyes_green", pref.g_eyes)
+	W.write("eyes_blue", pref.b_eyes)
+	W.write("b_type", pref.b_type)
+	W.write("disabilities", pref.disabilities)
+	W.write("organ_data", pref.organ_data)
+	W.write("rlimb_data", pref.rlimb_data)
+	W.write("body_markings", pref.body_markings)
+	W.write("body_descriptors", pref.body_descriptors)
+	W.write("bgstate", pref.bgstate)
 
 /datum/category_item/player_setup_item/physical/body/sanitize_character(var/savefile/S)
 	pref.r_hair			= sanitize_integer(pref.r_hair, 0, 255, initial(pref.r_hair))

NebulaSS13/Nebula

code/modules/client/preference_setup/general/01_basic.dm
@@ -9,19 +9,20 @@
 	name = "Basic"
 	sort_order = 1
 
-/datum/category_item/player_setup_item/physical/basic/load_character(var/savefile/S)
-	from_file(S["gender"],                pref.gender)
-	from_file(S["bodytype"],              pref.bodytype)
-	from_file(S["spawnpoint"],            pref.spawnpoint)
-	from_file(S["real_name"],             pref.real_name)
-	from_file(S["name_is_always_random"], pref.be_random_name)
+/datum/category_item/player_setup_item/physical/basic/load_character(datum/pref_record_reader/R)
+	pref.gender =         R.read("gender")
+	pref.bodytype =       R.read("bodytype")
+	pref.spawnpoint =     R.read("spawnpoint")
+	pref.real_name =      R.read("real_name")
+	pref.be_random_name = R.read("name_is_always_random")
 
-/datum/category_item/player_setup_item/physical/basic/save_character(var/savefile/S)
-	to_file(S["gender"],                  pref.gender)
-	to_file(S["bodytype"],                pref.bodytype)
-	to_file(S["spawnpoint"],              pref.spawnpoint)
-	to_file(S["real_name"],               pref.real_name)
-	to_file(S["name_is_always_random"],   pref.be_random_name)
+/datum/category_item/player_setup_item/physical/basic/save_character(datum/pref_record_writer/W)
+	W.write("gender",                pref.gender)
+	W.write("age",                   pref.age)
+	W.write("bodytype",              pref.bodytype)
+	W.write("spawnpoint",            pref.spawnpoint)
+	W.write("real_name",             pref.real_name)
+	W.write("name_is_always_random", pref.be_random_name)
 
 /datum/category_item/player_setup_item/physical/basic/sanitize_character()
 

code/modules/client/preference_setup/general/02_body.dm
@@ -29,64 +29,38 @@ var/global/list/valid_bloodtypes = list("A+", "A-", "B+", "B-", "AB+", "AB-", "O
 	sort_order = 2
 	var/hide_species = TRUE
 
-/datum/category_item/player_setup_item/physical/body/load_character(var/savefile/S)
-	from_file(S["species"], pref.species)
-	from_file(S["hair_colour"], pref.hair_colour)
-	from_file(S["facial_hair_colour"], pref.facial_hair_colour)
-	from_file(S["skin_colour"], pref.skin_colour)
-	from_file(S["eye_colour"], pref.eye_colour)
-	from_file(S["skin_tone"], pref.skin_tone)
-	from_file(S["hair_style_name"], pref.h_style)
-	from_file(S["facial_style_name"], pref.f_style)
-	from_file(S["b_type"], pref.b_type)
-	from_file(S["disabilities"], pref.disabilities)
-	from_file(S["organ_data"], pref.organ_data)
-	from_file(S["rlimb_data"], pref.rlimb_data)
-	from_file(S["body_markings"], pref.body_markings)
-	from_file(S["appearance_descriptors"], pref.appearance_descriptors)
-	from_file(S["bgstate"], pref.bgstate)
+/datum/category_item/player_setup_item/physical/body/load_character(datum/pref_record_reader/R)
+	pref.species =                R.read("species")
+	pref.hair_colour =            R.read("hair_colour")
+	pref.facial_hair_colour =     R.read("facial_hair_colour")
+	pref.skin_colour =            R.read("skin_colour")
+	pref.eye_colour =             R.read("eye_colour")
+	pref.skin_tone =              R.read("skin_tone")
+	pref.h_style =                R.read("hair_style_name")
+	pref.f_style =                R.read("facial_style_name")
+	pref.b_type =                 R.read("b_type")
+	pref.disabilities =           R.read("disabilities")
+	pref.organ_data =             R.read("organ_data")
+	pref.rlimb_data =             R.read("rlimb_data")
+	pref.body_markings =          R.read("body_markings")
+	pref.appearance_descriptors = R.read("appearance_descriptors")
+	pref.bgstate =                R.read("bgstate")
 
-	// Grandfathering in older saves post colour rewrite.
-	var/r = 0
-	var/g = 0
-	var/b = 0
-	if(!pref.hair_colour || pref.hair_colour == COLOR_BLACK)
-		from_file(S["hair_red"],     r)
-		from_file(S["hair_green"],   g)
-		from_file(S["hair_blue"],    b)
-		pref.hair_colour = rgb(r, g, b)
-	if(!pref.facial_hair_colour || pref.facial_hair_colour == COLOR_BLACK)
-		from_file(S["facial_red"],   r)
-		from_file(S["facial_green"], g)
-		from_file(S["facial_blue"],  g)
-		pref.facial_hair_colour = rgb(r, g, b)
-	if(!pref.skin_colour || pref.skin_colour == COLOR_BLACK)
-		from_file(S["skin_red"],     r)
-		from_file(S["skin_green"],   g)
-		from_file(S["skin_blue"],    b)
-		pref.skin_colour = rgb(r, g, b)
-	if(!pref.eye_colour || pref.eye_colour == COLOR_BLACK)
-		from_file(S["eyes_red"],     r)
-		from_file(S["eyes_green"],   g)
-		from_file(S["eyes_blue"],    b)
-		pref.eye_colour = rgb(r, g, b)
-	// End grandfathering.
-
-/datum/category_item/player_setup_item/physical/body/save_character(var/savefile/S)
-	to_file(S["species"], pref.species)
-	to_file(S["skin_tone"], pref.skin_tone)
-	to_file(S["hair_colour"], pref.hair_colour)
-	to_file(S["facial_hair_colour"], pref.facial_hair_colour)
-	to_file(S["skin_colour"], pref.skin_colour)
-	to_file(S["eye_colour"], pref.eye_colour)
-	to_file(S["hair_style_name"],pref.h_style)
-	to_file(S["facial_style_name"],pref.f_style)
-	to_file(S["b_type"], pref.b_type)
-	to_file(S["disabilities"], pref.disabilities)
-	to_file(S["organ_data"], pref.organ_data)
-	to_file(S["body_markings"], pref.body_markings)
-	to_file(S["appearance_descriptors"], pref.appearance_descriptors)
-	to_file(S["bgstate"], pref.bgstate)
+/datum/category_item/player_setup_item/physical/body/save_character(datum/pref_record_writer/W)
+	W.write("species",                pref.species)
+	W.write("skin_tone"],             pref.skin_tone)
+	W.write("hair_colour"],           pref.hair_colour)
+	W.write("facial_hair_colour"],    pref.facial_hair_colour)
+	W.write("skin_colour"],           pref.skin_colour)
+	W.write("eye_colour"],            pref.eye_colour)
+	W.write("hair_style_name",        pref.h_style)
+	W.write("facial_style_name",      pref.f_style)
+	W.write("b_type",                 pref.b_type)
+	W.write("disabilities",           pref.disabilities)
+	W.write("organ_data",             pref.organ_data)
+	W.write("body_markings",          pref.body_markings)
+	W.write("appearance_descriptors", pref.appearance_descriptors)
+	W.write("bgstate",                pref.bgstate)
 
 	var/list/rlimb_string_data = list()
 	for(var/limb in pref.rlimb_data)
@@ -94,7 +68,7 @@ var/global/list/valid_bloodtypes = list("A+", "A-", "B+", "B-", "AB+", "AB-", "O
 		if(ispath(model))
 			var/decl/prosthetics_manufacturer/model_data = GET_DECL(model)
 			rlimb_string_data[limb] = model_data.name
-	to_file(S["rlimb_data"], rlimb_string_data)
+	W.write("rlimb_data", rlimb_string_data)
 
 /datum/category_item/player_setup_item/physical/body/sanitize_character(var/savefile/S)
 