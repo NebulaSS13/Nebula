tiramisuapimancer/Pyrelight

Lowers base move delay on simple animals across the board to compensate for move decl delays.

code/modules/mob/living/simple_animal/_simple_animal.dm
@@ -17,7 +17,7 @@
 		/decl/move_intent/run/animal
 	)
 
-	var/base_movement_delay = 4
+	var/base_movement_delay = 0 // Added to the delaye expected from movement decls.
 	ai = /datum/mob_controller
 
 	var/can_have_rider = TRUE

NebulaSS13/Nebula

code/modules/mob/living/simple_animal/_simple_animal.dm
@@ -17,7 +17,7 @@
 		/decl/move_intent/run/animal
 	)
 
-	var/base_movement_delay = 4
+	var/base_movement_delay = 0 // Added to the delay expected from movement decls.
 	ai = /datum/mob_controller
 
 	var/can_have_rider = TRUE